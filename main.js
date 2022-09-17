f217a6585f64bc8749c88f8990dc4ca877739d74
ꜱʜᴜʙʜᴀ <93957744+ShubhaPokharel@users.noreply.github.com>
2022-09-17 10:09:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function take_Snapshot() {
     })
 }
 
+
 console.log('ml5 version:', ml5.version);
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Oot6t79z2/model.json', modelLoaded);
 
@@ -24,7 +25,7 @@ function speak() {
     var synth = window.speechSynthesis;
     speak_data_1 = "The first prediction is " + prediction_1;
     speak_data_2 = "The second prediction is " + prediction_2;
-    var utterThis = new SpeechSynthesisisUtterance(speak_data_1 + speak_data_2);
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
 }
 function check() {
